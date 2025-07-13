b12ff2473cc049c88100c17f52fb4c25dc7ba1f1
Usman S. <ui.sabuwala@gmail.com>
2025-07-13 13:12:34 +0000

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [tsConfigPaths(), tanstackStart({ target: "bun" }), tailwindcss()],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({ target: "bun", customViteReactPlugin: true }),
+    tailwindcss(),
+  ],
 });
